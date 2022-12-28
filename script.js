e600e338f1384e5997efe52bf38eb2171581d6ca
karthikeka <119919351+Karthikeka@users.noreply.github.com>
2022-12-28 20:58:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+  q1 :write a difference btwn http/1.1 vs http/2.0
+
+htttp/1.1   .
+Ithe usest works on the textual format
+slower compared to http/2
+basic header compression
+server push is not available
+loads resources one after another
+the default communication method is GET
+
+
+HTTP/2
+FAster compare to http/1.1
+loads resources parallely 
+server push is available
+advanced header compression
+no default communication method
